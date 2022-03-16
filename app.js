6f97b710176ec90f5fb9e14ef3d17d99b17cc66a
Isabel Rojas <isabel.rojasi@outlook.com>
2022-03-15 20:42:44 -0500

    update

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
